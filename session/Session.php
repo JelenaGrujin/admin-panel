1123fbfa9637562c9a5ac18f38f10e06c1267133
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-01-17 18:38:26 +0100

    Update Session.php

--- session/Session.php
+++ session/Session.php
@@ -2,7 +2,7 @@
 
 class Session{
 	
-	private $productinfo=array();
+	private $datainfo=array();
 	private $own_ses;
 	
 	public function __construct(){
@@ -10,39 +10,51 @@ class Session{
 			$this->onSession();
 		}
 		
-		
 	}
 	
-	public function onSession(){
+	private static function onSession(){
 		
 		session_start();
 		
 	}
+	
+	public function serialize($data){
+        return serialize($data);
+    }
 
-	public function productPause($productinfo){
-		$_SESSION['product'][]=
-	}
-//proveriti...
+    public function unserialize($data){
+        return unserialize($data);
+    }
+    
 	public function create_session($session_name, $ses_array=false){
 		if (!isset($_SESSION[$session_name])) {
 			if ($ses_array==false) {
-				$_SESSION[$session_name]=$array();
+				$_SESSION[$session_name]=array();
 			}else{
 				$_SESSION[$session_name]='';
 			}
 		}
-
-
 	}
 	
-	public function fillSession($session_name, $session_array){
+	public function fillSession($session_name, $datainfo){
 		
 		if (isset($_SESSION[$session_name])){
-			$_SESSION[$session_name][]=$session_array;
+			$_SESSION[$session_name][]=$datainfo;
 		}else {
 			$_SESSION[$session_name]=array();
 		}
 		
+	}
+	
+	public function getSessionData($session_name){
+
+		if (isset($_SESSION[$session_name])){
+		
+			return $_SESSION[$session_name];
+			
+		}else {
+			return false;
+		}
 	}
 	
 	public function offSession($session_name){
@@ -53,6 +65,15 @@ class Session{
 		}
 		
 	}
+	
+	public static function getInstance(){
+	    if(!self::$instance)
+	    {
+	      self::$instance = new self();
+	    }
+	   
+	    return self::$instance;
+	  }
 }
 
 
