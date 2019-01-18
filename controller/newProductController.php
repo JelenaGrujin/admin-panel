06c7cede0871e45a7ade11730f10536e48846dd2
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-01-18 13:25:50 +0100

    Update newProductController.php

--- controller/newProductController.php
+++ controller/newProductController.php
@@ -295,73 +295,11 @@ class newProductController extends Controller{
 	
 	public function pause(){
 		
-		$id_euro=isset($_POST['id_euro'])?$_POST['id_euro']:"";
-		$location_data_1=isset($_POST['location_data_1'])?$_POST['location_data_1']:"";
-		$location_data_2=isset($_POST['location_data_2'])?$_POST['location_data_2']:"";
-		$location_data_3=isset($_POST['location_data_3'])?$_POST['location_data_3']:"";
-		$addres_location=isset($_POST['addres_location'])?$_POST['addres_location']:"";
-		$adres_num=isset($_POST['adres_num'])?$_POST['adres_num']:"";
-		$number=isset($_POST['number'])?$_POST['number']:"";
-		$object=isset($_POST['object'])?$_POST['object']:"";
-		$flors=isset($_POST['flors'])?$_POST['flors']:"";
-		$of_flors=isset($_POST['of_flors'])?$_POST['of_flors']:"";
-		$price=isset($_POST['price'])?$_POST['price']:"";
-		$min_price=isset($_POST['min_price'])?$_POST['min_price']:"";
-		$deposit=isset($_POST['deposit'])?$_POST['deposit']:"";
-		$commission=isset($_POST['commission'])?$_POST['commission']:"";
-		$payment=isset($_POST['payment'])?$_POST['payment']:"";
-		$square=isset($_POST['square'])?$_POST['square']:"";
-		$surface_area=isset($_POST['surface_area'])?$_POST['surface_area']:"";
-		$equipment=isset($_POST['equipment'])?$_POST['equipment']:"";
-		$celing_height=isset($_POST['celing_height'])?$_POST['celing_height']:"";
-		$structure=isset($_POST['structure'])?$_POST['structure']:"";
-		$kitchen=isset($_POST['kitchen'])?$_POST['kitchen']:"";
-		$num_rooms=isset($_POST['br_soba'])?$_POST['br_soba']:"";
-		$num_bath=isset($_POST['kupatila'])?$_POST['kupatila']:"";
-		$num_wc=isset($_POST['num_wc'])?$_POST['num_wc']:"";
-		$num_terrace=isset($_POST['terasa'])?$_POST['terasa']:"";
-		$level=isset($_POST['level'])?$_POST['level']:"";
-		$salon_m=isset($_POST['salon_m'])?$_POST['salon_m']:"";
-		$sec=isset($_POST['security'])?$_POST['security']:array();
-		$num_elevator=isset($_POST['num_elevator'])?$_POST['num_elevator']:"";
-		$construc_year=isset($_POST['construc_year'])?$_POST['construc_year']:"";
-		$num_air_con=isset($_POST['num_air_con'])?$_POST['num_air_con']:"";
-		$num_garages=isset($_POST['num_garages'])?$_POST['num_garages']:"";
-		$note=isset($_POST['note'])?$_POST['note']:"";
-		$description=isset($_POST['description'])?$_POST['description']:"";
-		$active=isset($_POST['active'])?$_POST['active']:"";
-		$active_data=isset($_POST['active_data'])?$_POST['active_data']:"";
-		$info=isset($_POST['info'])?$_POST['info']:"";
-		$electricity=isset($_POST['electricity'])?$_POST['electricity']:"";
-		$network=isset($_POST['network'])?$_POST['network']:"";
-		$maintenance=isset($_POST['maintenance'])?$_POST['maintenance']:"";
-		$aces=isset($_POST['accessories'])?$_POST['accessories']:array();
-		$gar=isset($_POST['garage'])?$_POST['garage']:array();
-		$pro=isset($_POST['provider'])?$_POST['provider']:array();
-		$bath=isset($_POST['type_bath'])?$_POST['type_bath']:array();
-		$ter=isset($_POST['type_terrace'])?$_POST['type_terrace']:array();
-		$prod=isset($_POST['product_type'])?$_POST['product_type']:array();
-		$hea=isset($_POST['heating'])?$_POST['heating']:array();
-		$car=isset($_POST['carpentry'])?$_POST['carpentry']:array();
-		
-		$provider= implode(', ', $pro);
-		$type_bath=implode(', ', $bath);
-		$product_type=implode(', ', $prod);
-		$type_terrace=implode(', ', $ter);
-		$accessories=implode(', ', $aces);
-		$heating=implode(', ', $hea);
-		$carpentry=implode(', ', $car);
-		$security=implode(', ', $sec);
-		$garage=implode(', ', $gar);
-		
-		$session_name='product';		
-		
-		$datainfo=array($id_euro, $location_data_1, $location_data_2, $location_data_3, $addres_location, $adres_num, $number, $object, $flors, $of_flors, $price, $min_price, $deposit, $commission, $payment, $square, $surface_area, $equipment, $celing_height, $structure, $heating, $carpentry, $kitchen, $num_rooms, $num_bath, $num_wc, $num_terrace, $level, $salon_m, $security, $num_elevator, $construc_year, $num_air_con, $num_garages, $note, $description, $active, $active_data, $info, $electricity, $network, $maintenance, $accessories, $garage, $provider, $type_terrace, $type_bath, $product_type);
-		
+		$datainfo=self::getData();
 		if ($datainfo){
 
-			$this->sesia->create_session($session_name);
-			$this->sesia->fillSession($session_name, $datainfo);
+			$this->sesia->create_session('product');
+			$this->sesia->fillSession('product', $datainfo);
 			$this->protyco->showTypeProduct();
 			
 		}else {
@@ -372,9 +310,6 @@ class newProductController extends Controller{
 		}
 		
 	}
-	
-	
-	
 	public function showOwner(){
 		
 		$page_new_product='active';
