519044d21796d12235737b01c5965f19af2bbc34
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-01-30 12:50:58 +0100

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,15 +1,1 @@
-<?php
-	if(!isset($_SESSION['user'])) {
-		session_start();
-	}
-	
-	$ulogovan=unserialize($_SESSION['user']);
-	
-	if ($ulogovan) {
-?>
 
-<?php 
-	} else {
-		header('Location:login.php?msgg=Morate se ulogovti');
-	}
-?>
