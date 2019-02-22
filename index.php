b5307c10cfdb38318d271eec7d652a9611cb5a1c
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-02-22 13:08:45 +0100

    Update index.php
    Functions where I directly call 'view function' works, but if I call 'view func' over the Controller class, it's not working. It lunch all views in one page, or can't see object.

--- index.php
+++ index.php
@@ -2,7 +2,29 @@
 require_once 'vendor/autoload.php';
 
 use Admin\classes\Route;
+use Admin\controller\EquipmentController;
+use Admin\controller\LogController;
+use Admin\controller\Controller;
 
 
 
+Route::set('index.php', function(){
+    require_once 'view/login.php';
+});
+
+Route::set('home', function(){
+    $makemonth=date("d-m-Y", strtotime("+1 months"));
+    $makeweek=date("d-m-Y", strtotime("+1 week"));
+    $makeday=date("d-m-Y", strtotime("+1 day"));
+    $maked=date("d-m-Y", strtotime("-1 day"));
+    
+    $page_homepa = 'active';
+    include 'view/homefiles/home_link.php';
+});
+
+  //  Route::set('equipment', EquipmentController::showView('login'));
+    
+       // Route::set('type', Controller::showView('login'));
+        
+          //  Route::set('helo', LogController::login());
 ?>
