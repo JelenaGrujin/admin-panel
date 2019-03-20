7082c293d5ea15f0f95a2861079ee8c66e9ca83e
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-03-20 12:34:35 +0100

    Update Route.php

--- src/classes/Route.php
+++ src/classes/Route.php
@@ -1,29 +1,6 @@
 <?php
-namespace Admin\classes;
+$ruter->get('index.php', 'LogController::login()');
 
-class Route{
-   
-    public static $_routes=array();
-    
-    public static function set($route, $function) {
-        
-        $_routes[$route]=$function;
-        
-        if ($route==$_GET['url']) {
-            
-            $c=explode('::', $function);
-            $co=$c["0"];
-            $con="Admin\\controller\\$co";
-            $fun=$c['1'];
-            
-            
-            new $con;
-           
-            echo $con.'->'.$fun;
-        }
-       
-    }
-}
-
+$ruter->post('neka', 'cao.php');
 
 ?>
