27346efd684418215ebc0cb8a12b716de87423d1
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-03-27 12:42:34 +0100

    Update LogController.php

--- src/controller/LogController.php
+++ src/controller/LogController.php
@@ -1,81 +1,69 @@
-<?php 
+<?php
 namespace Admin\controller;
 
-use Admin\controller\Controller;
-use Admin\controller\HomeController;
 use Admin\classes\Login;
 use Admin\session\Session;
 use Admin\model\UserDao;
 
-class LogController extends Controller{
-	
-	protected $session_name='user';
-	public $user;
-	
-	public function __construct(){
-		
-		parent::__construct();
-		$this->daouser = new UserDao();
-		$this->home = new homeController();
-		$this->sesion = new Session();
-		$this->user = new Login();
-	}
-	
-	public function showView(){
-		
-		 include 'view/login.php';
-	}
-	
-	public function login(){
+use function Composer\Autoload\includeFile;
+
+class LogController {
+    
+    protected $session_name='user';
+    public $user;
+    public $daouser;
+    public $session;
+    public $home;
+
+    
+    public function __construct(){
+
+        $this->session = new Session();
+        $this->daouser = new UserDao();
+        $this->user = new Login();
+        $this->home = new HomeController();
+    }
+    
+    public function showView(){
         
-        $username=isset($_POST['username'])?$_POST['username']:"";
-        $password=isset($_POST['password'])?$_POST['password']:"";
+        include 'view/login.php';
+      
+    }
+    
+    public function login(){
+     
+        $this->user->setUsername($_POST['username']?$_POST['username']:"");
+        $this->user->setPassword($_POST['password']?$_POST['password']:"");
         
-        $this->user->setUsername($username);
-        $this->user->setPassword($password);
+        $user=$this->daouser->selectUserByUsernameAndPassword($this->user->getUsername(), $this->user->getPassword());
         
-        $user=self::get_data();
+        if (!empty($user)){
+            
+            $this->session->create_session($this->session_name);
+            $this->session->serializeSession($this->session_name, $user);
+        }
+        
+        self::redirect();
+    }
+
+    public function redirect() {
+
+        
+        if ($this->session->sessionExist($this->session_name)==true){
+            
+            $this->home->showHome();
+        }else {
+            header('Location:login.php?$msgg=morate se ulogovati');
+            
+        }
+    }
+    
+    public function logout(){
+        session_unset();
+        session_destroy();
+        header('Location:login.php');
         
     }
-	
-	public function get_data(){
-		
-		
-		$username=$this->user->getUsername();
-		$password=$this->user->getPassword();
-		
-		$pass=$this->user->salted($password);
-
-		$user=$this->daouser->selectUserByUsernameAndPassword($username, $pass);
-			if ($user!==null){
-				
-				$this->sesion->create_session($this->session_name);
-				$this->sesion->serializeSession($this->session_name, $user);
-		}
-		
-		self::redirect();
-		
-	}
-	
-	public function redirect() {
-		
-		if ($this->sesion->sessionExist($this->session_name)==true){
-			
-			$this->home->showHome();
-		}else {
-			header('Location:login.php?$msgg=morate se ulogovati');
-			
-		}
-	}
-	 
-	public function logout(){
-
-		session_unset();
-		session_destroy();
-		header('Location:login.php');
-		
-	}
-	
+    
 }
-
 ?>
