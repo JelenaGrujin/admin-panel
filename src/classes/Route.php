055bcf45c1d8c848c6bdb150bedd600ed8c5361f
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-03-29 20:27:09 +0100

    Update Route.php

--- src/classes/Route.php
+++ src/classes/Route.php
@@ -2,6 +2,6 @@
 $ruter->get('index.php', 'LogController::showView');
 $ruter->post('home', 'LogController::login');
 $ruter->get('logout', 'LogController::logout');
-$ruter->get('products', 'ProductController::showProduct');
+$ruter->get('product', 'ProductController::showProduct');
 $ruter->post('type', 'TypeProductController::showTypeProduct');
 ?>
