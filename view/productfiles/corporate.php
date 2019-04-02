b157f8b159b8ebe3dcc1020af086a8febd2ac6ed
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-04-02 14:17:27 +0200

    Update corporate.php

--- view/productfiles/corporate.php
+++ view/productfiles/corporate.php
@@ -1,13 +1,7 @@
 <?php
-	if(!isset($_SESSION['user'])) {
-		session_start();
-	}
-	
-	$ulogovan=unserialize($_SESSION['user']);
 	$msg=isset($msg)?$msg:"";
 	$id_product=isset($id_product)?$id_product:"";
-	$errors =isset($errors)?$errors:array();
-	if ($ulogovan) {	
+	$errors =isset($errors)?$errors:array();	
 ?>
 	<form class="form" action="routes.php" method="POST" enctype="multipart/form-data">
 	<div class="row mt-4 pb-3 bg-light text-danger border border-primary">
@@ -181,8 +175,3 @@
 </div>
 
 		</form>
-<?php 
-	} else {
-		header('Location:login.php?msgg=You need to log in');
-	}
-?>
