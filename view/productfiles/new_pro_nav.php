ef0d928f3aa5816d14926904dca09e6963e16f70
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-04-02 14:02:14 +0200

    Update new_pro_nav.php

--- view/productfiles/new_pro_nav.php
+++ view/productfiles/new_pro_nav.php
@@ -1,15 +1,10 @@
 <?php
-	if(!isset($_SESSION['user'])) {
-		session_start();
-	}
-	
-	$ulogovan=unserialize($_SESSION['user']);
+
 	$msg=isset($msg)?$msg:"";
 	$page_inf = isset($page_info)?$page_info:"";
 	$page_own = isset($page_owner)?$page_owner:"";
 	$page_pho = isset($page_photos)?$page_photos:"";
 
-	if ($ulogovan) {
 ?>
 <span class="text-dark"><?php if (!empty($msg)) 
 echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">'.$msg.'<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -20,19 +15,14 @@ echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
 	<div class="card-header pl-2 pr-2">
 		<ul class="nav nav-tabs card-header-tabs">
 		  	<li role="presentation" class="nav-item" >
-		    	<a class="nav-link text-primary <?php echo $page_inf;?>" href="routes.php?page=new_product">About</a>
+		    	<a class="nav-link text-primary <?php echo $page_inf;?>" href="new_product">About</a>
 		  	</li>
 		  	<li role="presentation" class="nav-item" >
-		   	 	<a class="nav-link text-primary <?php echo $page_own;?>" href="routes.php?page=owner">Owner</a>
+		   	 	<a class="nav-link text-primary <?php echo $page_own;?>" href="owner">Owner</a>
 		  	</li>
 		  	<li role="presentation" class="nav-item">
-		    	<a class="nav-link text-primary <?php echo $page_pho;?>" href="routes.php?page=photos">Photos</a>
+		    	<a class="nav-link text-primary <?php echo $page_pho;?>" href="photos">Photos</a>
 		  	</li>
 		</ul>
 	</div>
 </div>
-<?php 
-	} else {
-		header('Location:login.php?msgg=You need to log in');
-	}
-?>
