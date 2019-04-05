a77311d65089b790e20fb1d4dd4a8ef760e7ea44
JelenaGrujin <jelenagrujinbps@gmail.com>
2019-04-05 14:17:33 +0200

    new

---
+++ src/controller/ProductTypeController.php
@@ -0,0 +1,47 @@
+<?php
+namespace Admin\controller;
+
+use Admin\controller\Controller;
+use Admin\model\typeDao;
+
+class ProductTypeController{
+
+	public function __construct(){
+
+		$this->daotype = new TypeDao();
+
+	}
+
+	public function showTypeProduct(){
+		
+		$typeslist=$this->daotype->selectFromProductType();
+		
+		$page_basepa='active';
+		$page_type_product = 'active';
+		$container = 'container';
+		include 'view/basefiles/base_link.php';
+		
+	}
+	
+	public function newProductType(){
+		
+		$name_type=isset($_POST['new_type'])?$_POST['new_type']:"";
+
+		$this->daotype->insertIntoProductType($name_type);
+		
+		$typeslist=$this->daotype->selectFromProductType();
+		
+		$msg='Successfully';
+		
+		$page_basepa='active';
+		$page_type_product = 'active';
+		$container = 'container';
+		include 'view/basefiles/base_link.php';
+		
+	}
+
+
+
+}
+
+?>
