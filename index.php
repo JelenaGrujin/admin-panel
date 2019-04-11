3f4ce93fde22f96b0150acda37015bd7d7afd84b
Jelena <39518565+JelenaGrujin@users.noreply.github.com>
2019-04-11 12:33:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-include 'view/header.php';
+include 'header.html';
 require_once 'vendor/autoload.php';
 
 use Admin\classes\Request;
@@ -8,5 +8,5 @@ use Admin\classes\Request;
 $ruter = Admin\classes\Router::insert('Route.php');
 
 $ruter->direct(Request::url(), Request::method());
-include 'view/footer.php';
+include 'footer.html';
 ?>
